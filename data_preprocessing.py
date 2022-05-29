518bbeffe3fddcb6eaa8211cd63bbf3e88abd2f1
Iamback-03 <earnstripes03@gmail.com>
2022-05-29 18:22:52 +0530

    Added ml model code

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,11 +3,12 @@ print("adding all the data preprocessing code related fraud detection")
 
 a=5
 b=7
-
 def sum(a,b):
-    c=a+b
-    return a
+    c = a +b
+    return c
 
-print("added a new function sum which is not correct")
+def multipication(a,b):
+    d= a+b
+    return d
 
-
+print("multipication function")    
