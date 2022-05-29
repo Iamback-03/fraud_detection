c9f4e730b13d442f81eb858ef3f53b69a580bef2
Iamback-03 <earnstripes03@gmail.com>
2022-05-29 16:14:50 +0530

    added a new function m

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,1 +1,13 @@
 print("adding all the data preprocessing code related fraud detection")
+
+
+a=5
+b=7
+
+def sum(a,b):
+    c=a+b
+    return a
+
+print("added a new function sum which is not correct")
+
+
